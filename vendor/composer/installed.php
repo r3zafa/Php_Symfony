5293cf337f4a0136edd7a371bc750f2dc30a9e96
r3zafa <r3zafaraji@gmail.com>
2021-08-01 13:30:20 -0700

    first symfony project updated 1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b69a2ec91e57efe5331e56249533509e0d52ca11',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b69a2ec91e57efe5331e56249533509e0d52ca11',
+            'dev_requirement' => false,
+        ),
+        'symfony/requirements-checker' => array(
+            'pretty_version' => 'v2.0.0',
+            'version' => '2.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/requirements-checker',
+            'aliases' => array(),
+            'reference' => 'e3d5565eb69a4a2195905c8669f32e988c8e6be0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
